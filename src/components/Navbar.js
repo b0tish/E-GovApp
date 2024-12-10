c8466d3275efbda0860518355ab0978b0271c86a
b0tish <maharjan.saugat1@gmail.com>
2024-12-10 12:18:15 +0545

    Initial Commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../css/Navbar.css";
+
+// const items = ["Home", "About Us", "Contact Us"];
+
+function Navbar() {
+  return (
+    <>
+      <nav>li*</nav>
+    </>
+  );
+}
