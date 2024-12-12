d0b546074e9ea9cdec0695c009091724dafa2541
NickName-AM <maharjanebik061@gmail.com>
2024-12-12 22:34:21 +0545

    Local Government Allocation Form

---
+++ src/components/LocalGovernmentAllocationForm.js
@@ -0,0 +1,15 @@
+const LocalGovernmentAllocationForm = () => {
+    const provinces = ["bagmati", "karnali", "gandaki"];
+    const localGovernment = [
+        {}
+    ]
+
+
+    return ( 
+        <div className="w-28 p-4 rounded-lg shadow-md">
+            
+        </div>
+     );
+}
+ 
+export default LocalGovernmentAllocationForm;
