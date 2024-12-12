73d5bae1bd94fdbece61ceb19488c52b59072096
NickName-AM <maharjanebik061@gmail.com>
2024-12-12 22:35:15 +0545

    dummy navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,7 @@
-import React from "react";
-import "../css/Navbar.css";
-
-
-
-// function Navbar(props) {
-//   var items = [
-//     { name: props.title1},
-//     {name: props.title2}
-    
-//   ];
-
- function returning(props) {
-    var thisis = props.map((item) => <li id={item}>{item}</li>); // Use item.name directly
-  }
-
-  return (
-    <ul>
-      
-    </ul>
-  );
-
-
+const Navbar = () => {
+  return ( 
+    <></>
+   );
+}
+ 
 export default Navbar;
