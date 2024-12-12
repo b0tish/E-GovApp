21ed019a5b2ba7120eb3828c3f32ff571dcb263b
ursche <urjalapariyar@gmail.com>
2024-12-12 13:56:12 +0545

    Ict Hawa

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+function Home()
+{
+    return(
+        <>
+            <div className="container">
+                
+            </div>
+        </>
+
+    );
+}
+export default Home;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,25 @@
 import React from "react";
 import "../css/Navbar.css";
+var items = [
+  { name: props.title1},
+  {name: props.title2}
+]
 
 
-
-// function Navbar(props) {
-//   var items = [
-//     { name: props.title1},
-//     {name: props.title2}
+ function Navbar(props) {
+  fu
+  
+    return (
+      <ul>
+        
+      </ul>
+    );
     
-//   ];
-
- function returning(props) {
-    var thisis = props.map((item) => <li id={item}>{item}</li>); // Use item.name directly
   }
 
-  return (
-    <ul>
-      
-    </ul>
-  );
+ 
+
+  
 
 
 export default Navbar;
