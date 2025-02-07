c95aa1bd78d2e25a2e38c7d233936fcad4bc4484
NickName-AM <maharjanebik061@gmail.com>
2025-02-08 01:37:52 +0545

    [=] adds description field in allocation forms w/ modifications

--- src/components/ProvinceAllocationForm.js
+++ src/components/ProvinceAllocationForm.js
@@ -1,21 +1,30 @@
 import { useState } from "react";
 
 const ProvinceAllocationForm = () => {
-    const ministries = ["Ministry of Education", "Ministry of Health and Population", "Ministry of Defence"];
+    const ministries = [
+        {"name": "Ministry of Education", "budget": 150000}, 
+        {"name": "Ministry of Health and Population", "budget": 400000}, 
+        {"name": "Ministry of Defence", "budget": 300000}, 
+    ];
+
     const provinces = ["bagmati", "karnali", "gandaki"];
+
+    const [budget, setBudget] = useState(ministries[0]["budget"]);
 
     // const [districts, setdistricts] = useState(districtsProvince[provinces[0]]);
 
     return ( 
-        <div className="h-screen flex justify-center items-center">
-            <div className="w-[500px] p-8 rounded-xl border-2 border-gray-200">
+        <div className="w-screen min-h-screen flex justify-center items-center">
+            <div className="w-1/2 p-8 rounded-xl border-2 border-gray-200">
             <div className="text-3xl font-bold">Budget Allocation</div>
             <form method="POST">
                 <div className="pt-6">
                     <label className="font-bold block mb-2" htmlFor="ministry">Ministry</label>
-                    <select className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" id="ministry">
+                    <select className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" id="ministry" onChange={e => {
+                        setBudget(ministries.filter(ministry => ministry["name"] === e.target.value)[0]["budget"])
+                    }}>
                         {ministries.map(ministry => (
-                            <option value={ministry}>{ministry}</option>
+                            <option value={ministry["name"]}>{ministry["name"]}</option>
                         ))}
                     </select>
                 </div>
@@ -30,8 +39,20 @@ const ProvinceAllocationForm = () => {
                 </div>
 
                 <div className="pt-6">
+                    <label className="font-bold block mb-2" htmlFor="ministry">Remaining Budget</label>
+                    <div className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" id="ministry">
+                        { budget }
+                    </div>
+                </div>
+
+                <div className="pt-6">
                     <label className="font-bold block mb-2" htmlFor="budget">Budget</label>
-                    <input className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" type="number" id="budget" placeholder="In Rupees. (Ex: 1000000)" />
+                    <input className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" type="number" step={10000} id="budget" placeholder="In Rupees. (Ex: 1000000)" />
+                </div>
+
+                <div className="pt-6">
+                    <label className="font-bold block mb-2" htmlFor="description">Description</label>
+                    <textarea className="w-full p-3 border-2 border-gray-200 rounded-lg bg-white" rows={10} id="description" ></textarea>
                 </div>
 
                 <div className="pt-6">
