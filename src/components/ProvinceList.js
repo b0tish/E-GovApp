0700fdabd44b04e7efad80d6f2facc39fd8576b3
b0tish <maharjan.saugat1@gmail.com>
2025-02-09 18:25:09 +0545

    [+] Adding delete and update function

--- src/components/ProvinceList.js
+++ src/components/ProvinceList.js
@@ -5,7 +5,51 @@ import { Link } from "react-router-dom";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-function ProvinceList({ provinces }) {
+function ProvinceList({ provinces, setProvinces }) {
+  const handleProvinceDelete = async (provinceId) => {
+    try {
+      console.log("This is the province list id:", provinceId);
+      const response = await fetch(
+        `http://localhost:5000/province/${provinceId}`,
+        {
+          method: "DELETE",
+        },
+      );
+
+      if (response.ok) {
+        setProvinces((prevProvinces) =>
+          prevProvinces.filter((province) => province._id !== provinceId),
+        );
+      } else {
+        console.error("Error deleting province: ", response.status);
+      }
+    } catch (error) {
+      console.error("Error");
+    }
+  };
+
+  const handleProvinceUpdate = async (provinceId) => {
+    console.log(provinceId);
+    try {
+      const response = await fetch(
+        `http://localhost:5000/province/${provinceId}`,
+        {
+          method: "PUT",
+        },
+      );
+
+      if (response.ok) {
+        setProvinces((prevProvinces) =>
+          prevProvinces.filter((province) => province._id !== provinceId),
+        );
+      } else {
+        console.error("Error deleting province: ", response.status);
+      }
+    } catch (error) {
+      console.error("Error");
+    }
+  };
+
   return (
     <>
       {provinces.map((province) => {
@@ -45,6 +89,42 @@ function ProvinceList({ provinces }) {
           <Card key={province._id} className="mx-2 my-2">
             <CardContent className="text-center">
               {province.name}
+              <span className="flex flex-row-reverse">
+                <button onClick={() => handleProvinceDelete(province._id)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"
+                    />
+                  </svg>
+                </button>
+
+                <button onClick={() => handleProvinceUpdate(province._id)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+                    />
+                  </svg>
+                </button>
+              </span>
+
               <div className="h-[300px] flex items-center justify-center">
                 <Pie data={data} options={options} />
               </div>
