9644f4ec1adb3ea00980b4e9bc0246d60465503e
b0tish <maharjan.saugat1@gmail.com>
2025-02-24 13:40:30 +0545

    [+] test changes

--- src/pages/HomePrivate.js
+++ src/pages/HomePrivate.js
@@ -6,6 +6,7 @@ import { useAuth } from "../components/AuthContext";
 function HomePrivate() {
   const { isLoggedIn } = useAuth();
   console.log(isLoggedIn);
+  consolee.log(isAdmin);
   return (
     <div className="p-20">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
